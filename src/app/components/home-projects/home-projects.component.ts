fc8099ba33e6ecbbb1028e3938fd532cc4a2a68b
ltmariusz <ltmariuszl@gmail.com>
2024-03-08 20:38:05 +0100

    editing url not complited

--- src/app/components/home-projects/home-projects.component.ts
+++ src/app/components/home-projects/home-projects.component.ts
@@ -1,41 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 export interface ProjectsArray {
-  name: string,
-  description: string,
-  img?: string,
-  technology?: string,
-  url?: string
+  name: string;
+  description: string;
+  img?: string;
+  technology?: string;
+  url?: string;
 }
 
 @Component({
   selector: 'app-home-projects',
   templateUrl: './home-projects.component.html',
-  styleUrls: ['./home-projects.component.scss']
+  styleUrls: ['./home-projects.component.scss'],
 })
 export class HomeProjectsComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
-
-  listOfProject: Array<ProjectsArray> = new Array()
+  listOfProject: Array<ProjectsArray> = new Array();
 
   ngOnInit(): void {
     this.listOfProject = [
       {
-        name: "Cv (to)",
-        description: "strona przedstawiająca informacje o mnie",
+        name: 'Cv (to)',
+        description: 'strona przedstawiająca informacje o mnie',
         img: 'icon-code.svg',
-        technology: "Angular",
-        url:"mariuszlemanski.com"
+        technology: 'Angular',
+        url: 'mariuszlemanski.com',
       },
       {
-        name: "FrontendoweABC",
-        description: "wskazówki dla początkujacych związane z html/css/js",
+        name: 'FrontendoweABC',
+        description: 'wskazówki dla początkujacych związane z html/css/js',
         img: 'fABCN.png',
-        technology: "html/css/js",
-        url:"brak"
-      }
-    ]
+        technology: 'html/css/js',
+        url: 'https://papaya-empanada-c964c9.netlify.app/',
+      },
+    ];
   }
 
+  redrictTo(url: string) {
+    this.router.navigate([url]);
+  }
 }
