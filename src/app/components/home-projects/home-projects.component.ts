3d4c73b6402278fda6e77e2211506e77bd0f5523
ltmariusz <96800290+ltmariusz@users.noreply.github.com>
2024-07-22 12:39:36 +0000

    backgroud of job changing

--- src/app/components/home-projects/home-projects.component.ts
+++ src/app/components/home-projects/home-projects.component.ts
@@ -17,7 +17,7 @@ export interface ProjectsArray {
   styleUrls: ['./home-projects.component.scss'],
 })
 export class HomeProjectsComponent implements OnInit {
-  constructor(private router: Router) {}
+  constructor(private router: Router) { }
 
   listOfProject: Array<ProjectsArray> = new Array();
 
@@ -44,11 +44,35 @@ export class HomeProjectsComponent implements OnInit {
       },
     ];
     for (let i = 0; i < this.listOfProject.length; i++) {
-      this.giveColorToShadow(i, this.listOfProject[i].color!);
+
     }
   }
+  getColor(project:any) {
+    if (project.isHovered) {
+      return this.lightenColor(project.color, 0.8); // Adjust 0.8 to change the intensity of lightening
+    }
+    return "#ebebeb";
+  }
 
-  giveColorToShadow(index: number, color: string) {}
+  lightenColor(color: string, factor: number): string {
+    let c = parseInt(color.substring(1), 16);
+
+    let r = (c >> 16) & 0xFF;
+    let g = (c >> 8) & 0xFF;
+    let b = c & 0xFF;
+
+    // Linear interpolation towards white (255, 255, 255)
+    r = Math.round(r + factor * (255 - r));
+    g = Math.round(g + factor * (255 - g));
+    b = Math.round(b + factor * (255 - b));
+
+    return `#${this.componentToHex(r)}${this.componentToHex(g)}${this.componentToHex(b)}`;
+  }
+
+  componentToHex(c: number): string {
+    const hex = c.toString(16);
+    return hex.length == 1 ? '0' + hex : hex;
+  }
 
   redrictTo(url: string) {
     window.open(url, '_blank');
